a10e26ff8ebeb891ec6e9db73dc94a2acfe9fe64
sbadajoz <sbadajoz@student.cccd.edu>
2017-10-24 15:10:40 -0700

    Create timer

--- app/src/main/java/edu/orangecoastcollege/cs273/alohamusic/SplashActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/alohamusic/SplashActivity.java
@@ -1,7 +1,11 @@
 package edu.orangecoastcollege.cs273.alohamusic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+
+                Intent intent = new Intent(SplashActivity.this, MusicActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
+
     }
 }
